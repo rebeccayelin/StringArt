787f42d2ff77d709967bb4a3664011a433c1c30b
rebeccayelin <rebeccalin@alumni.ubc.ca>
2023-04-30 22:49:13 -0400

    update examples

--- example_dog.m
+++ example_dog.m
@@ -11,7 +11,7 @@ stringArt( ...
     'outputFileNamePrefix', 'dog', ... % prefix of the output files
     'invertInput', true, ... % false -> reconstruct white area, true -> reconstruct black area
     'invertOutput', true, ... % false -> white string, true -> black string
-    'numPins', 256, ... % number of pins that are placed on the frame (default is 256)
+    'numPins', 200, ... % number of pins that are placed on the frame (default is 256)
     'threadThickness', 0.15, ... % physical thickness of the thread in mm (default is 0.15)
     'frameDiameter', 614.4, ... % physical diameter of the circular frame in mm (default is 614.4)
     'pinSideLength', 2, ... % physical side length of a pin with quadratic cross section in mm (default is 2)
